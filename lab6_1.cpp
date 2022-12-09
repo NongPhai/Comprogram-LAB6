989a90501aea2afe5640236b45229b277e4470cb
phai <norapat_c@cmu.ac.th>
2022-12-09 16:22:02 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main(){
     int x=1;
     int y=0;
-    int z=0; 
+    int a=0; 
     while(x != 0){
     cout << "Enter an integer: ";
     cin >> x;
     if(x%2 == 0){
         y++;
     }else{
-        z++;
+        a++;
     }
     }
     cout << "#Even numbers = " << y-1 <<"\n" ;
-    cout << "#Odd numbers = " << z ;
+    cout << "#Odd numbers = " << a ;
     return 0;
 }
